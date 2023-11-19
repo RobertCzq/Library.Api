662f8cea4657c7ef3b2e4cf476f2dbacc0004aa4
RobertCzq <cazacu.robert5@gmail.com>
2023-11-19 22:16:38 +0100

    chore: add deployment to azure

--- Library.Api/Program.cs
+++ Library.Api/Program.cs
@@ -27,10 +27,11 @@ builder.Services.AddScoped<IMembersService, MembersService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseSwagger();
+app.UseSwaggerUI();
+
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
 }
 
 app.UseAuthorization();
